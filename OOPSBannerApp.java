0a311423f2744cf5df50de7d01aecbe3f3bfbd59
niyati negi <niyatinegi@niyatis-MacBook-Air-5.local>
2026-02-18 10:38:06 +0530

    UC2: Implemented 7-line ASCII banner for OOPS

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,15 +1,22 @@
 /**
  * OOPSBannerApp
  *
- * UC1: Print literal text "OOPS"
+ * UC2: Print "OOPS" as a 7-line ASCII banner
  *
  * @author Niyati
- * @version 1.0
+ * @version 2.0
  */
 
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println(" *****    *****    *****   ***** ");
+        System.out.println("*     *  *     *  *     *  *     ");
+        System.out.println("*     *  *     *  *     *  *     ");
+        System.out.println("*     *  *     *  *****    ***** ");
+        System.out.println("*     *  *     *  *        *     ");
+        System.out.println("*     *  *     *  *        *     ");
+        System.out.println(" *****    *****   *        ***** ");
     }
 }
