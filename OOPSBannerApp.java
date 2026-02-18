048038e16a68be566da53af630ae1ce9906d3026
niyati negi <niyatinegi@niyatis-MacBook-Air-5.local>
2026-02-18 10:34:22 +0530

    UC1: Implemented Print OOPS functionality

---
+++ OOPSBannerApp.java
@@ -0,0 +1,15 @@
+/**
+ * OOPSBannerApp
+ *
+ * UC1: Print literal text "OOPS"
+ *
+ * @author Niyati
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
